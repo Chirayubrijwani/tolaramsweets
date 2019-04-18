a69e2db00cf3f046fdbbf9af83545bbd7f1bbf60
chirayu brijwani <chirayubrijwani203@gmail.com>
2019-04-18 22:23:47 +0530

    'dwedwe'

--- tolaramsweets/settings.py
+++ tolaramsweets/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1nj70sdz@wuw#x6)$l3(t_y(x_u$-_n8a=1q8970%%j1!*e5ry'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
